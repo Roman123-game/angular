1bbf0f54027c6dfa9271994f114e1004981d0642
RB <82284311+Roman123-game@users.noreply.github.com>
2023-10-19 20:26:31 +0200

    update 3 files and create 4 files

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input} from '@angular/core';
+import { IProduct } from 'src/app/models/products';
+
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  // styleUrls: ['./app.component.css']
+})
+
+export class ProductComponent {
+  @Input() product: IProduct
+}
